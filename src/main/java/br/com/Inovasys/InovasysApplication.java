c5f6c108a31f9038e03fd72883a56cf28dd76a18
Pedro Henrique de Borba <PedroHenriquede.Borba@philips.com>
2026-02-17 21:02:12 -0300

    inicio do sonho Inovasys

---
+++ src/main/java/br/com/Inovasys/InovasysApplication.java
@@ -0,0 +1,13 @@
+package br.com.Inovasys;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InovasysApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(InovasysApplication.class, args);
+	}
+
+}
